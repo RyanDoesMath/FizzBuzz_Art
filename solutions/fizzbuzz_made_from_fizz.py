f4e9ec4e2cf92b3beb949bbd6acdb50d6dd73505
Ryan Folks <rfolks92@gmail.com>
2022-08-24 11:01:37 -0400

    Update fizzbuzz_made_from_fizz.py

--- solutions/fizzbuzz_made_from_fizz.py
+++ solutions/fizzbuzz_made_from_fizz.py
@@ -1,9 +1,11 @@
 # A one line solution that also spells the word fizz.
 
-print([''.join((ix, x)          [1]) if ''.join((            ix, x)[1]) !=       '' else (ix, x)
+print(
+      [''.join((ix, x)          [1]) if ''.join((            ix, x)[1]) !=       '' else (ix, x)
       [0]                            +1                               for                ix, 
       x in enumerate(                zip                           ([''               if 
       i%3                            !=                          0                 else             
      'fizz'                          for                       i                 in       
      range                           (1,                     101                )]      
-     ,[''                      if i%5 != 0 else           'buzz' for i in      range(1, 101)]))])
+     ,[''                      if i%5 != 0 else           'buzz' for i in      range(1, 101)]))]
+)
